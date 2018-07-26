537e403a0a2777d7b042d78a3c1d126dc7b924b8
Justincadburywong <justincadburywong@gmail.com>
2018-07-25 22:24:34 -0700

    add dress code, favicon, edit JS counter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ;(function () {
-	
+
 	'use strict';
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
@@ -10,7 +10,7 @@
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -78,11 +78,11 @@
 				}
 			}
 
-		});	
+		});
 
 	}
 
-	
+
 
 	// Click outside of the Mobile Menu
 	var mobileMenuOutsideClick = function() {
@@ -104,7 +104,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -117,14 +117,14 @@
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
 	};
-	
+
 	var stickyBanner = function() {
 		var $stickyElement = $('.sticky-banner');
 		var sticky;
@@ -135,41 +135,41 @@
 		  })
 		}
 	};
-
-	// Set the date we're counting down to
-	var countDownDate = new Date("Dec 28, 2017 15:37:25").getTime();
-
-	// Update the count down every 1 second
-	var x = setInterval(function() {
-
-	// Get todays date and time
-	var now = new Date().getTime();
-
-	// Find the distance between now an the count down date
-	var distance = countDownDate - now;
-
-	// Time calculations for days, hours, minutes and seconds
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-	// Display the result in an element with id="demo"
-	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-	// + minutes + "Minutes " + seconds + "Seconds ";
-
-	// Display the result in an element with id="demo"
-	document.getElementById("days").innerHTML = days +" <small>days</small>";
-	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
-
-	// If the count down is finished, write some text 
-	if (distance < 0) {
-	 clearInterval(x);
-	 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
-	}
-	}, 1000);
+	// 
+	// // Set the date we're counting down to
+	// var countDownDate = new Date("Sat Aug 18, 2018 17:30:00 GMT-0700 (Pacific Daylight Time)").getTime();
+	//
+	// // Update the count down every 1 second
+	// var x = setInterval(function() {
+	//
+	// // Get todays date and time
+	// var now = new Date().getTime();
+	//
+	// // Find the distance between now an the count down date
+	// var distance = countDownDate - now;
+	//
+	// // Time calculations for days, hours, minutes and seconds
+	// var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	// var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	// var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	//
+	// // Display the result in an element with id="demo"
+	// // document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
+	// // + minutes + "Minutes " + seconds + "Seconds ";
+	//
+	// // Display the result in an element with id="demo"
+	// document.getElementById("days").innerHTML = days +" <small>days</small>";
+	// document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+	// document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
+	// document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+	//
+	// // If the count down is finished, write some text
+	// if (distance < 0) {
+	//  clearInterval(x);
+	//  document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+	// }
+	// }, 1000);
 
 	// Document on load.
 
